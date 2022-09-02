485e194058c3a66581a328c940df5b8f54ff425f
inganyoyo <inganyoyo@me.com>
2022-09-02 11:11:13 +0900

    hello boot

---
+++ hello-springboot-mvn/src/main/java/com/example/hellospringbootmvn/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.hellospringbootmvn;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String test() {
+        return "Hello World! v1";
+    }
+}
---
+++ hello-springboot-mvn/src/test/java/com/example/hellospringbootmvn/HelloSpringbootMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbootmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootMvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
